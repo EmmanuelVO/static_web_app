4ceb0e11a899ef2c063448599070a12bb1e0b368
EmmanuelVO <83371337+EmmanuelVO@users.noreply.github.com>
2023-11-07 02:17:14 +0000

    limpieza

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 function App() {
   const title = 'Titulo chingon';
   
+  
+
   return (
     <div>
       <h1>{title}</h1>
       <input type="text" />
+      <button>Enviar</button>
+      <button>Cancelar</button>
     </div>
   );
+
 
 }
 
